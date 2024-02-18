993673ec2f80837f54c158c65c0330fa9250f04f
ghadakileni <kilenighada@gmail.com>
2024-02-18 20:45:12 +0100

    Catégories

--- app/components/Head.tsx
+++ app/components/Head.tsx
@@ -5,15 +5,15 @@ import { Ionicons } from '@expo/vector-icons';
 const HeaderPage= () => {
     
     return (
-      <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
-        <View style={styles.container}>
-          <View style={styles.topContainer}>
-            <View style={styles.leftContainer}>
-              <Ionicons color={'#242C5D'} name="menu-outline" size={24} />
-              <Text style={styles.greetingText}>Hello, le nom</Text>
+      <SafeAreaView style             = {{ flex: 1, backgroundColor: '#fff' }}>
+        <View style                   = {styles.container}>
+          <View style                 = {styles.topContainer}>
+            <View style               = {styles.leftContainer}>
+              <Ionicons color         = {'#242C5D'} name="menu-outline" size={24} />
+              <Text style             = {styles.greetingText}>Hello, le nom</Text>
             </View>
-            <TouchableOpacity style={styles.notificationIcon}>
-              <Ionicons color={'#242C5D'} name="notifications-outline" size={24} />
+            <TouchableOpacity style   = {styles.notificationIcon}>
+              <Ionicons color         = {'#242C5D'} name="notifications-outline" size={24} />
             </TouchableOpacity>
           </View>
           </View>
@@ -23,27 +23,27 @@ const HeaderPage= () => {
   
   const styles = StyleSheet.create({
     container: {
-      backgroundColor: '#fff',
-      paddingHorizontal: 25,
-      paddingTop: 20,
+      backgroundColor                 : '#fff',
+      paddingHorizontal               : 25,
+      paddingTop                      : 20,
     },
     topContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      marginBottom: 10,
+      flexDirection                   : 'row',
+      justifyContent                  : 'space-between',
+      alignItems                      : 'center',
+      marginBottom                    : 10,
     },
     leftContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
+      flexDirection                   : 'row',
+      alignItems                      : 'center',
     },
     greetingText: {
-      marginLeft: 10,
-      color: '#242C5D',
+      marginLeft                      : 10,
+      color                           : '#242C5D',
     },
     notificationIcon: {
-      flexDirection: 'row',
-      alignItems: 'center',
+      flexDirection                   : 'row',
+      alignItems                      : 'center',
     },
   
   });
--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,105 +1,137 @@
 import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { StyleSheet } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 import Colors from '@/constants/Colors';
 import { Link } from 'expo-router';
+import { ScrollView } from 'react-native-gesture-handler';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHome} from '@fortawesome/free-solid-svg-icons';
+import { faThLarge, faUtensils, faSwatchbook, faPeopleCarry } from '@fortawesome/free-solid-svg-icons';
+import { TouchableWithoutFeedback } from 'react-native';
 
 
 const categories = [
-    {
-        name : 'Tout',
-        icon : '',
-    },
-    {
-        name : 'Restauration',
-        icon : '',
-    },
-    {
-        name : 'Design',
-        icon : '',
-    },
-    {
-        name : 'Aide à domicile',
-        icon : '',
-    },
-    {
-        name : 'Livraison',
-        icon : '',
-    },
+    { name: 'Tout', icon:  faThLarge   },
+    { name: 'Restauration', icon: faUtensils },
+    { name: 'Design', icon: faSwatchbook },
+    { name: 'Aide à domicile', icon: faHome},
+    { name: 'Livraison', icon: faPeopleCarry },
 ];
-
 
 
 
 const Search = () => {
     return (
-      <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
-        <View style={styles.container}>
-          <Text style={styles.title}>Trouver un job</Text>
-          <View style={styles.searchContainer}>
-            <View style={styles.searchWrapper}>
-              <TouchableOpacity style={styles.searchIcon}>
-                <Ionicons color={'#242C5D'} name='search' size={15} />
+      <SafeAreaView style               = {{ flex: 1, backgroundColor: '#fff' }}>
+        <View style                     = {styles.container}>
+          <Text style                   = {styles.title}>Trouver un job</Text>
+          <View style                   = {styles.searchContainer}>
+            <View style                 = {styles.searchWrapper}>
+              <TouchableOpacity style   = {styles.searchIcon}>
+                <Ionicons color         = {'#242C5D'} name='search' size={15} />
               </TouchableOpacity>
-              <Text style={styles.searchText}>EX. Vendeur</Text>
+              <Text style               = {styles.searchText}>EX. Vendeur</Text>
             </View>
-            <TouchableOpacity style={styles.filterBtn}>
-              <Ionicons color={'#242C5D'} name="options-outline" size={15} />
+            <TouchableOpacity style     = {styles.filterBtn}>
+              <Ionicons color           = {'#242C5D'} name="options-outline" size={15} />
             </TouchableOpacity>
           </View>
-          </View>
-      </SafeAreaView>
+ <Text style  = {styles.title}>Catégories</Text>
+
+          <View style = {styles.list}>
+                {categories.map((item, index) => (
+        <TouchableOpacity key={index} style={{ marginRight: 20 }}>
+
+    <View style = {styles.circles}>
+            <FontAwesomeIcon size={20} icon={item.icon}  style={{ borderWidth: 0 }} color='#242C5D'
+ />
+        </View>
+
+        <Text style={styles.iconText}>{item.name}</Text>
+        </TouchableOpacity>
+    ))}
+</View>
+
+</View>
+
+     </SafeAreaView>
+     
     );
   };
   
   const styles = StyleSheet.create({
     container: {
-      backgroundColor: '#fff',
-      paddingHorizontal: 25,
-      paddingBottom: 30,
+      backgroundColor                   : '#fff',
+      paddingHorizontal                 : 25,
+      paddingBottom                     : 30,
     },
  
     topContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      marginBottom: 10,
+      flexDirection                     : 'row',
+      justifyContent                    : 'space-between',
+      alignItems                        : 'center',
+      marginBottom                      : 10,
     },
     leftContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
+      flexDirection                     : 'row',
+      alignItems                        : 'center',
     },
 
     title: {
-        color: '#242C5D',
-        fontWeight: 'bold',
-        marginBottom: 10,
+        color                           : '#242C5D',
+        fontWeight                      : 'bold',
+        marginTop : 15,
+        marginBottom                    : 10,
       },
     searchContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      borderColor: '#F3F4FB',
-      borderWidth: StyleSheet.hairlineWidth,
-      backgroundColor: '#F3F4FB',
-      borderRadius: 8,
+      flexDirection                     : 'row',
+      justifyContent                    : 'space-between',
+      alignItems                        : 'center',
+      borderColor                       : '#F3F4FB',
+      borderWidth                       : StyleSheet.hairlineWidth,
+      backgroundColor                   : '#F3F4FB',
+      borderRadius                      : 8,
     },
     searchWrapper: {
-      flexDirection: 'row',
-      alignItems: 'center',
+      flexDirection                     : 'row',
+      alignItems                        : 'center',
     },
     searchIcon: {
-      marginRight: 20,
-      marginLeft: 10,
+      marginRight                       : 20,
+      marginLeft                        : 10,
     },
     searchText: {
-      fontFamily: 'Red-Hat-Text',
-      fontSize: 12,
-      color: '#767C9A',
+      fontFamily                        : 'Red-Hat-Text',
+      fontSize                          : 12,
+      color                             : '#767C9A',
     },
     filterBtn: {
-      padding: 10,
+      padding                           : 10,
+    },
+    list:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 16,
+        marginTop: 10,
+    },
+    circles:{
+        borderColor : '#242C5D',
+        borderWidth : 1,
+        backgroundColor   : '#F3F4FB',
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    iconText: {
+        marginTop: 5,
+        fontSize: 12,
+        color: '#242C5D',
+        textAlign: 'center',
     },
   });
   
