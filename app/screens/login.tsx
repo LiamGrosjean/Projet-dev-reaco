bf9d5617ede1fbdee6c6834ec73c08901fa72342
LiamGrosjean <grosjeanliam@gmail.com>
2024-02-29 20:56:52 +0100

    Squashed commit of the following:
    commit 2124620f7f821565c7347afcbc4c55885720e6a5
    Author: LiamGrosjean <grosjeanliam@gmail.com>
    Date:   Thu Feb 29 20:56:12 2024 +0100
    
        login fonctionnel
    
        petite erreur de padding avec logo sur page login
    
        a rajouter checkbox se souvenir de moi
    
    commit 1df983ca3018514ba7fc78bdfe15ba8c4262b320
    Author: LiamGrosjean <grosjeanliam@gmail.com>
    Date:   Thu Feb 29 16:12:55 2024 +0100
    
        commit2, final
    
    commit d744b269c949dba3351c4d02b51017fb1a997bdd
    Author: LiamGrosjean <grosjeanliam@gmail.com>
    Date:   Thu Feb 29 11:51:28 2024 +0100
    
        test1

--- app/screens/login.tsx
+++ app/screens/login.tsx
@@ -1,12 +1,125 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import Colors from '@/constants/Colors';
+import React from 'react';
+import { View, Text, StyleSheet, Image, Touchable } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { useWarmUpBrowser } from '../hooks/useWarmUpBrowser';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { useOAuth } from '@clerk/clerk-expo';
+import { useRouter } from 'expo-router';
 
-const page = () => {
-  return (
-    <View>
-      <Text>log in</Text>
-    </View>
-  )
+enum Strategy {
+  Google = 'oauth_google',
+  Apple = 'oauth_apple',
 }
 
-export default page
+
+const Page = () => { // Name the component for better understanding
+  useWarmUpBrowser();
+
+  const router = useRouter();
+  const { startOAuthFlow: googleAuth } = useOAuth({ strategy: 'oauth_google' });
+  const { startOAuthFlow: appleAuth } = useOAuth({ strategy: 'oauth_apple' });
+  
+  const onSelectAuth = async (strategy: Strategy) => {
+    const selectedAuth = {
+      [Strategy.Google]: googleAuth,
+      [Strategy.Apple]: appleAuth,
+    }[strategy];
+
+    try {
+      const { createdSessionId, setActive } = await selectedAuth();
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+        router.back();
+      }
+    } catch (err) {
+      console.error('OAuth error', err);
+    }
+  };
+  
+  return (
+    <View style={styles.container}>
+      <View style={{paddingTop: 44}}>
+         <Image source={require('@/assets/images/Logo.png')} style={{width: 146, objectFit: 'contain', backgroundColor: 'red', height: 38}} />
+      </View>
+        <Text style={{color: Colors.light.primary, fontSize: 24, fontWeight: '500', marginTop: 58}}>Connexion</Text>
+        <Text style={{color: Colors.light.primary, fontSize: 14, fontWeight: '400', lineHeight: 17, marginTop: 22}}>Lorem ipsum dolor sit amet consectetur. Placerat pharetra sit nulla nisl rutrum orci </Text>
+        <View style={{marginTop: 52, gap: 31}}>
+            <View style={styles.labelContainer}>
+                <Text style={styles.label}>adresse mail</Text>
+                <TextInput autoCapitalize='none' placeholder='votrenom@univ.fr' style={styles.email} />
+            </View>
+            <View style={styles.labelContainer}>
+                <Text style={styles.label}>mot de passe</Text>
+                <TextInput autoCapitalize='none' placeholder='***************' style={styles.email} />
+            </View>
+                <TouchableOpacity style={styles.buttonWrapper}>
+                    <Text style={styles.buttonText}>Valider</Text>
+                </TouchableOpacity>
+        </View>
+        <View style={{marginTop: 41, gap:10}}>
+              <TouchableOpacity style={styles.btnWrapperOutline} onPress={() => onSelectAuth(Strategy.Google)}>
+                  <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', gap: 20}}>
+                    <FontAwesome5 name="google" size={22} color={Colors.light.primary} />
+                    <Text style={{fontSize: 16, fontWeight: 600, color: Colors.light.primary}}>Connexion avec Google</Text>
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.btnWrapperOutline} onPress={() => onSelectAuth(Strategy.Apple)}>
+                  <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', gap: 20}}>
+                    <FontAwesome5 name="apple" size={22} color={Colors.light.primary} />
+                    <Text style={{fontSize: 16, fontWeight: 600, color: Colors.light.primary}}>Connexion avec Apple</Text>
+                  </View>
+                </TouchableOpacity>
+        </View>
+    </View>
+  );
+};
+export default Page
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 33,
+    backgroundColor: Colors.light.background,
+  },
+  labelContainer: {
+    gap: 8,
+    paddingBottom: 8,
+    borderBottomColor: Colors.light.primary,
+    borderBottomWidth: 0.5,
+  },
+  label: {
+    color: '#242C5D',
+    fontSize: 12,
+    fontWeight: '300',
+    textTransform: 'uppercase',
+  },
+  email: {
+    color: '#242C5D',
+    fontSize: 14,
+    fontWeight: '600',
+  },
+  buttonWrapper: {
+    backgroundColor: Colors.light.accent,
+    paddingVertical: 14,
+    marginHorizontal: 'auto',
+    alignItems: 'center',
+    borderRadius: 5,
+    marginTop: 41,
+  },
+  btnWrapperOutline: {
+    paddingVertical: 14,
+    marginHorizontal: 'auto',
+    alignItems: 'center',
+    borderRadius: 5,
+    borderColor: Colors.light.primary,
+    borderWidth: 1,
+  },
+  buttonText: {
+    color: Colors.light.background,
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
+
