906815e4477b685930974c05b9b977a46d2394b5
LiamGrosjean <grosjeanliam@gmail.com>
2024-02-29 21:08:26 +0100

    ajout composant btn

---
+++ app/components/button.tsx
@@ -0,0 +1,32 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import Colors from '@/constants/Colors'
+
+const button = (btnText: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined) => {
+  return (
+    <View>
+        <TouchableOpacity style={styles.buttonWrapper}>
+            <Text style={styles.buttonText}>{btnText}</Text>
+        </TouchableOpacity>
+    </View>
+  )
+}
+
+export default button
+
+const styles = StyleSheet.create({
+    buttonText: {
+        color: Colors.light.background,
+        fontSize: 16,
+        fontWeight: '600',
+      },
+      buttonWrapper: {
+        backgroundColor: Colors.light.accent,
+        paddingVertical: 14,
+        marginHorizontal: 'auto',
+        alignItems: 'center',
+        borderRadius: 5,
+        marginTop: 41,
+      },
+})
